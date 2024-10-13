f40a6f3fa82dda7a78b271dd6cdd2a22d5d07696
raouia <raouia.daghnouj@esprit.tn>
2024-10-13 21:08:49 +0100

    Sauvegarde de mes modifications

--- resources/views/collectes/show.blade.php
+++ resources/views/collectes/show.blade.php
@@ -12,12 +12,6 @@
             </div>
         </div>
 
-        <a href="{{ route('collectes.edit', $collecte->id) }}" class="btn btn-warning mt-3">Modifier</a>
-        <form action="{{ route('collectes.destroy', $collecte->id) }}" method="POST" style="display:inline;">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger mt-3">Supprimer</button>
-        </form>
         <a href="{{ route('collectes.index') }}" class="btn btn-secondary mt-3">Retour à la Liste</a>
     </div>
 </div>
