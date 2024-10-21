521e12866373fcad94ee1c2431cfdc9319d27ed1
Ghazi <ghazibouzid63@gmail.com>
2024-10-21 22:50:31 +0100

    commit

--- resources/views/layouts/commonMaster.blade.php
+++ resources/views/layouts/commonMaster.blade.php
@@ -30,7 +30,7 @@
     var channel = pusher.subscribe('popup-channel');
     channel.bind('user-register', function(data) {
      
-      toastr.success(JSON.stringify(data.name) + ' has been created');
+      alert(JSON.stringify(data.message) + ' has been created');
     });
   </script>
   
